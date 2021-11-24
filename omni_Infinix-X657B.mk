702f401a0b9c2f88cadf61d34f8a4ab56782872f
smallpp420 <78730004+SmallPP420@users.noreply.github.com>
2021-11-24 19:47:39 +0000

    Update omni_Infinix-X657B.mk

--- omni_Infinix-X657B.mk
+++ omni_Infinix-X657B.mk
@@ -17,16 +17,13 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from Infinix-X657B device
 $(call inherit-product, device/infinix/Infinix-X657B/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := Infinix-X657B
